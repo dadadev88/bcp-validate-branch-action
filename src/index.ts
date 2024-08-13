60a22d974e860f54d14e6d219ea07df86a97d138
dadadev <dadadev88@gmail.com>
2024-08-12 23:37:47 -0500

    Beta version 0.0.2

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,21 @@ const Environments = {
 }
 
 try {
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`[BCP-BV] The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`[BCP-BV] The event payload: ${payload}`);
 
-  const context = JSON.stringify(github.context, undefined, 2)
-  console.log(`[BCP-BV] The context: ${context}`);
+  // const context = JSON.stringify(github.context, undefined, 2)
+  // console.log(`[BCP-BV] The context: ${context}`);
 
-  const branchName = core.getInput('environment');
-  if (branchName === Environments.CERT) {
+  const environment = core.getInput('environment');
+  if (environment === Environments.CERT) {
     if (github.context.eventName !== 'pull_request')
       core.setFailed('[BCP-BV] Environment is CERT, require Pull Request');
   }
-
-  if (branchName === Environments.PROD) {
+  console.log({ environment });
+  if (environment === Environments.PROD) {
     const rc = core.getInput('rc');
+    console.log({ rc });
     if (!rc.startsWith('RC-CERT'))
       core.setFailed('Required RC branch/tag. This should begin with "RC-CERT..."');
   }
